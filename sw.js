ed5004f7ff8011a459301694bbf0cbc6faef9623
Prashanth <Prashanth@shoptreeapp.com>
2020-07-24 16:08:09 +0530

    Add test Service worker

--- sw.js
+++ sw.js
@@ -1,35 +1,59 @@
-// Cache the assets
-const staticDevCoffee = "prashanth-cafe"
-const assets = [
-  "./",
-  "./pwa.html",
-  "./css/pwa/style.css",
-  "./pwa.js",
-  "./images/fakeprofile/bitbucket.png",
-  "./images/fakeprofile/bootstrap.png",
-  "./images/fakeprofile/css.png",
-  "./images/fakeprofile/github.png",
-  "./images/fakeprofile/html.png",
-  "./images/fakeprofile/js.png",
-  "./images/fakeprofile/laravel.png",
-  "./images/fakeprofile/php.png",
-  "./images/fakeprofile/react.png",
-  "./images/fakeprofile/swift.png",
+var APP_PREFIX = 'PrashanthCafe'  // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'v1.0'  // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var REPO_BASE_URL = 'https://prashanthspatil.github.io/'
+var URLS = [                            // Add URL you want to cache in this list.
+  REPO_BASE_URL,                        // If you have separate JS/CSS files,
+  REPO_BASE_URL+'index.html'            // add path to those files here
 ]
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
   )
 })
 
-// Fetch data from cache when offline
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
     })
   )
 })
