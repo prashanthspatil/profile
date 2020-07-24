e043d4c4c745fee90fdf831f8258ad5c46ee22b7
Prashanth <Prashanth@shoptreeapp.com>
2020-07-24 20:42:23 +0530

    Include Toast

--- pwa.js
+++ pwa.js
@@ -29,11 +29,19 @@ const showCoffees = () => {
   container.innerHTML = output
 }
 
+function showToast(message) {
+  var x = document.getElementById("toast");
+  x.className = "show";
+  x.innerHTML = message;
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
 window.isUpdateAvailable = new Promise(function(resolve, reject) {
 	if ('serviceWorker' in navigator) {
 		// register service worker file
 		navigator.serviceWorker.register('sw.js')
 			.then(reg => {
+				showToast('Service worker registered');
 				reg.onupdatefound = () => {
 					const installingWorker = reg.installing;
 					installingWorker.onstatechange = () => {
@@ -51,15 +59,19 @@ window.isUpdateAvailable = new Promise(function(resolve, reject) {
 					};
 				};
 			})
-			.catch(err => console.error('[SW ERROR]', err));
+			.catch(err => {
+				showToast('[SW ERROR]', err);
+				console.error('[SW ERROR]', err)});
 	}
 });
 
 window['isUpdateAvailable']
 	.then(isAvailable => {
 		if (isAvailable) {
+			showToast('New Update Available');
 			console.log('New Update available!');
 		} else {
+			showToast('No Update Available');
 			console.log('No Update available!');
 		}
 	});
