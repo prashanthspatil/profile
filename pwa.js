05b5a814edf265dc4b3b55ef80ca8c3f622e1578
Prashanth <Prashanth@shoptreeapp.com>
2020-07-24 14:28:44 +0530

    Include PWA

---
+++ pwa.js
@@ -0,0 +1,38 @@
+const container = document.querySelector(".container")
+const technologies = [
+  { name: "Perspiciatis", image: "images/fakeprofile/bitbucket.png" },
+  { name: "Voluptatem", image: "images/fakeprofile/css.png" },
+  { name: "Explicabo", image: "images/fakeprofile/html.png" },
+  { name: "Rchitecto", image: "images/fakeprofile/php.png" },
+  { name: " Beatae", image: "images/fakeprofile/github.png" },
+  { name: " Vitae", image: "images/fakeprofile/laravel.png" },
+  { name: "Inventore", image: "images/fakeprofile/php.png" },
+  { name: "Veritatis", image: "images/fakeprofile/react.png" },
+  { name: "Accusantium", image: "images/fakeprofile/bootstrap.png" },
+]
+
+const showCoffees = () => {
+  let output = ""
+  technologies.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  )
+  container.innerHTML = output
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees)
---
+++ serviceworker.js
@@ -0,0 +1,34 @@
+// Cache the assets
+const staticDevCoffee = "prashanth-cafe"
+const assets = [
+  "/",
+  "/pwa.html",
+  "/css/pwa/style.css",
+  "/pwa.js",
+  "/images/fakeprofile/bitbucket.png",
+  "/images/fakeprofile/bootstrap.png",
+  "/images/fakeprofile/css.png",
+  "/images/fakeprofile/github.png",
+  "/images/fakeprofile/html.png",
+  "/images/fakeprofile/js.png",
+  "/images/fakeprofile/laravel.png",
+  "/images/fakeprofile/php.png",
+  "/images/fakeprofile/react.png"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+// Fetch data from cache when offline
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
