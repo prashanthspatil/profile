e56118fbbd26e0f43f2fc76056641550e3cceed9
Prashanth <Prashanth@shoptreeapp.com>
2020-07-24 18:13:10 +0530

    Remove console log

--- sw.js
+++ sw.js
@@ -20,14 +20,14 @@ var URLS = [                            // Add URL you want to cache in this lis
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
+  // console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
+        // console.log('responding with cache : ' + e.request.url)
         return request
       } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
+        // console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
 
